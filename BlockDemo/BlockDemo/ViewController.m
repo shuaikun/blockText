ad509dc36312d71f19b5f0f83b0fb04e929c1e51
shuaikun <2440312922@qq.com>
2016-03-21 17:48:29 +0800

    test
    111

--- BlockDemo/BlockDemo/ViewController.m
+++ BlockDemo/BlockDemo/ViewController.m
@@ -28,10 +28,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UILabel *lable = [[UILabel alloc]init];
-    [self.view addSubview:lable];
-    
-    
     //1:代码创建的
     
 //    LYButton * btn = [LYButton buttonWithType:UIButtonTypeSystem];
